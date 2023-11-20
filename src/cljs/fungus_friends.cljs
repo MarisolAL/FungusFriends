5772695dd0b33fb7b8dc5832164d017f0a9c2974
Marisol Amezcua <marisolamezcua@ciencias.unam.mx>
2023-11-20 13:20:46 -0600

    WIP: Fixes page style

--- src/cljs/fungus_friends.cljs
+++ src/cljs/fungus_friends.cljs
@@ -1,6 +1,7 @@
 (ns fungus-friends
   (:require
    [components.leaflet-map.views :as l-map]
+   [components.select :refer [select]]
    [re-frame.core :as rf]
    [reagent.core :as rg]
    [subs :as subs]))
@@ -18,8 +19,29 @@
                                :attribution "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"}]}]]))
 
 (defn page []
-  [:div.main-container.title-message
-   [:h1 {:class "dark-blue"} "Welcome Fungus Friends!"]
-   [:p {:class "dark-blue"} "Here you have your fungus"]
-   [:p "Selects here"]
-   [fungus-map]])
+  (rg/with-let [select-1-atm (rg/atom nil)
+                select-2-atm (rg/atom nil)]
+    [:div.main-container
+     [:h1 {:class "dark-blue"} "Welcome Fungus Friends!"]
+     [:p {:class "dark-blue"} "Here you have your fungus"]
+     [:div.welcome-page__map-container
+      [fungus-map]]
+     [:div.welcome-page__selects-container
+      [select {:label       "Select 1"
+               :placeholder "Select"
+               :options     [{:label "Option 1"
+                              :value :option-1}
+                             {:label "Option 2"
+                              :value :option-2}]
+               :value       @select-1-atm
+               :on-change   #(reset! select-1-atm %)
+               :clearable   true}]
+      [select {:label       "Select 2"
+               :placeholder "Select"
+               :options     [{:label "Option 1"
+                              :value :option-1}
+                             {:label "Option 2"
+                              :value :option-2}]
+               :value       @select-2-atm
+               :on-change   #(reset! select-2-atm %)
+               :clearable   true}]]]))
